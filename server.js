d0dd17ca1cdf610671c388207ad4ab4e13df74ca
Ankush Milan <milanankush4@gmail.com>
2022-05-11 12:43:44 +0530

    second commit

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    // origin: "http://localhost:3000",
-    origin: "*",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
